3951bf8f0c060679a0c64b350d59b9e1a9c4fe31
Eilison98 <in05013300@gmail.com>
2022-07-11 17:09:54 +0900

    자료형

---
+++ Python_Study/Bool_DataType/Python_bool.py
@@ -0,0 +1,15 @@
+# 불 자료형이란?
+
+# 불(bool) 자료형이란 참(True)과 거짓(False)을 나타내는 자료형이다. 불 자료형은 다음 2가지 값만을 가질 수 있다.
+
+# True - 참
+# False - 거짓
+
+# 다음과 같이 변수 a에는 True를, 변수 b에는 False를 지정해 보자.
+# >>> a = True
+# >>> b = False
+
+# >>> type(a)
+# <class 'bool'>
+# >>> type(b)
+# <class 'bool'>
